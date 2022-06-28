23390941eb516c4c19295ea624c08c65fffd5d6e
Angus Nimmo <78160632+angusnimmo@users.noreply.github.com>
2022-06-28 12:14:06 +0100

    Add files via upload

---
+++ 031.py
@@ -0,0 +1,10 @@
+coins = [1, 2, 5, 10 , 20, 50, 100, 200]
+total = 200
+ways = [0]*201
+ways[0] = 1
+
+for i in range(len(coins)):
+    for j in range(coins[i], total + 1):
+        ways[j] += ways[j - coins[i]]
+
+print(ways[-1])
