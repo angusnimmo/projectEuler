ec8eba7c8942d914ebedf44c95050d9768d08c2e
Angus Nimmo <78160632+angusnimmo@users.noreply.github.com>
2022-07-16 20:38:23 +0100

    Add files via upload

---
+++ 079.py
@@ -0,0 +1,19 @@
+import requests
+import itertools
+
+def passcodeCheck(passcode):
+    for i in keylogs:
+        for j in range(3):
+            for k in range(j, 3):
+                if passcode.index(i[j]) > passcode.index(i[k]):
+                    return False
+                
+    return True
+
+if __name__ == "__main__":
+    keylogs = requests.get('https://projecteuler.net/project/resources/p079_keylog.txt').text
+    keylogs = keylogs.split('\n')
+    keylogs = keylogs[:-1]
+    digits = ''.join(set(''.join(keylogs)))
+    passcodes = [''.join(i) for i in itertools.permutations(digits)]
+    print([i for i in passcodes if passcodeCheck(i)])
